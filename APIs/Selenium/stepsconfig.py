75f0091823c398064a5aa97f2641d318218a727a
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2024-02-26 19:27:53 +0530

    added dockerfile, default chrome driver

--- APIs/Selenium/stepsconfig.py
+++ APIs/Selenium/stepsconfig.py
@@ -1,5 +1,6 @@
 import os
+from pathlib import Path as pp
 
 
 class SeleniumClientConf():
-  DRIVER_PATH = os.getenv('DRIVER_PATH', "")
+  DRIVER_PATH = os.getenv('DRIVER_PATH', str(pp(pp(__file__).parent.parent.parent, "chrome-driver/chromedriver")))
