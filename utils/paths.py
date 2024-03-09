5daded9dd1da0d0d747a486a420df58104a69278
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2024-03-09 22:55:18 +0530

    added paths

---
+++ utils/paths.py
@@ -0,0 +1,15 @@
+from pathlib import Path as pp
+
+
+__here = pp(__file__)
+
+TA = __here.parent.parent
+
+APIS = pp(TA, 'APIs')
+BASE = pp(TA, 'base')
+CONFIG = pp(TA, 'config')
+DATA_HANDLER = pp(TA, 'data_handler')
+PATTERNS = pp(TA, 'patterns')
+TEMPLATE_IMAGES = pp(TA, 'template_images')
+OTHERS = pp(TA, 'others')
+UTILS = pp(TA, 'utils')
