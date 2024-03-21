4d6cda8dce361b2894c473b20f17271e3415132c
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2024-03-21 13:13:20 +0530

    pi: running in headliess, commented out pyautogui, added usernames in examples/collectuserdata

--- examples/collect_tweet_data.py
+++ examples/collect_tweet_data.py
@@ -1,12 +1,6 @@
 from APIs.Selenium.selenium_steps import CollectUserTweetData
 from APIs.Selenium.selenium_steps import Login
 
-right = ["zoo_bear", "RoflGandhi_", 'asadowaisi', "dhruv_rathee", "TheDeshBhakt", "khanumarfa", "ShyamMeeraSingh", "_sayema", "Baajis1"]
-wrong = ["MrSinha_", "SureshChavhanke", "SushantBSinha", "SushantBSinha", "KapilMishra_IND", "SudarshanNewsTV", "TigerRajaSingh", "noconversion", "TeamHinduOrg"]
-
 Login(botname="default_bot")()
 
-
-for user in right:
-  CollectUserTweetData(user_profile=f"https://twitter.com/{user}", number_of_tweets=5000, file_name=user)()
-
+CollectUserTweetData(user_profile=f"https://twitter.com/X", number_of_tweets=5000, file_name='X')()
