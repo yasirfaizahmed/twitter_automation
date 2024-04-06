a2ef0bf6bd4694608fb1c4501af3beb7531ff03f
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2024-04-06 17:53:00 +0530

    metapath

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@ export DRIVER_PATH='/usr/bin/chromedriver'
 
 export PYTHONPATH='/home/pi/Documents/python_codes/twitter_automation'
 
+export METADATA='/home/pi/Documents/secrets/bot_metadata.json'
+
 source .venv/bin/activate
