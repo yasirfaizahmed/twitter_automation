0e65846937997bc53411c33ad9d0429aa24919e1
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2024-04-09 22:53:14 +0530

    stable script

--- automation/hadith_post_fajr/create_image.py
+++ automation/hadith_post_fajr/create_image.py
@@ -11,7 +11,7 @@ from pathlib import PosixPath
 import time
 from datetime import date
 import random
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter
 
 from utils.paths import RESOURCES, GENERATED
 from log_handling.log_handling import logger
@@ -20,11 +20,16 @@ IMAGE_SET_DIR = "hadith_backgroud_image_set"
 IMAGE_SET_PATH = pp(RESOURCES, IMAGE_SET_DIR)
 FONT_SET_DIR = "fonts"
 FONT_SET_PATH = pp(RESOURCES, FONT_SET_DIR)
-BRIGHTNESS = 40  # out of 100
+BRIGHTNESS = 30  # out of 100
 BLURR = 0  # out of 100
 # DEFAULT_FONT = "AnotherFlight-m752"
 # DEFAULT_FONT = "MemorialLane-z8XVX"
 DEFAULT_FONT = "RobotoSlab-VariableFont_wght"
+# DEFAULT_FONT = "Oswald-Medium"
+MIN_IMAGE_WIDTH = 500
+MIN_IMAGE_HEIGHT = 700
+GOOD_QUOTE_HEIGHT = 0.4  # 40% of the image height
+MAX_ITER_COUNT = 20
 
 
 def handle_output_dir() -> bool:
@@ -39,12 +44,17 @@ def pick_random_image() -> PosixPath:
 	all_images = list(IMAGE_SET_PATH.iterdir())
 	random_image = random.choice(all_images)
 	logger.info(f"chose image {random_image.absolute()} randomly")
+	# width, height = Image.open(random_image)
+
 	return random_image
 
 
-def pick_first_image() -> PosixPath:
+def pick_first_image(image: str) -> PosixPath:
+	if pp(image).exists():
+		logger.info(f"using image {pp(image).absolute()}")
+		return pp(image)
 	first_image = list(IMAGE_SET_PATH.iterdir())[0]
-	logger.info(f"using image {first_image.absolute()}")
+	logger.info(f"using first image {first_image.absolute()}")
 	return first_image
 
 
@@ -56,13 +66,94 @@ def get_font_file(font_name: str = DEFAULT_FONT) -> PosixPath:
 	return pp(FONT_SET_PATH, f"{font_name}.ttf")
 
 
-def generate_image(content: str, author: str, random: bool = True):
+def custom_image_generator(
+	quote,
+	author,
+	fg,
+	image_path="",
+	font_path="",
+	font_size=20,
+	size=(),
+	watermark_text="",
+	watermark_font_size: int = None,
+	font_size_author: int = None,
+):
+	image_path: PosixPath = (
+		pick_random_image() if image_path == "" else pick_first_image(image_path)
+	)
+	font_path: PosixPath = get_font_file() if font_path == "" else font_path
+
+	bg = ImgObject(image=str(image_path.absolute()), brightness=BRIGHTNESS, blur=BLURR)
+	image = Image.open(bg.image)
+	width, height = image.size if size == () else size
+	image.resize((width, height))  #
+	enhancer = ImageEnhance.Brightness(image)
+	image = enhancer.enhance(bg.brightness / 100)
+	if bg.blur != 0:
+		image = image.filter(ImageFilter.BoxBlur(bg.blur))
+	draw = ImageDraw.Draw(image)
+
+	quote_height = font_size  # just to initialize
+	iter_count = 0
+	while (quote_height < (GOOD_QUOTE_HEIGHT * height)) or (
+		quote_height > (GOOD_QUOTE_HEIGHT * height)
+	):
+		# check what must be the font size so that the text part takes up correct % of space in middle
+		font = ImageFont.truetype(str(font_path.absolute()), font_size)
+		lines = []
+		line = ""
+		for word in quote.split():
+			line_width = draw.textsize(line + " " + word, font)[0]
+			if line_width > width - 40:
+				lines.append(line)
+				line = word
+			else:
+				line += " " + word
+
+		lines.append(line)
+		quote_height = sum([draw.textsize(line, font)[1] for line in lines])
+		if iter_count > MAX_ITER_COUNT:
+			break
+		if quote_height < (GOOD_QUOTE_HEIGHT * height):
+			font_size += 1  # increase font size
+		elif quote_height > (GOOD_QUOTE_HEIGHT * height):
+			font_size -= 1  # decreset font size
+		iter_count += 1
+
+	y = (height - quote_height - font_size) // 2
+
+	for line in lines:
+		line_width = draw.textsize(line, font)[0]
+		x = (width - line_width) // 2
+		draw.text((x, y), line, fg, font=font, antialias=True)
+		y += draw.textsize(line, font)[1]
+
+	dash_width = draw.textsize(" - ", font)[0]
+	x = (width - dash_width) // 2
+	y += font_size // 2
+	# draw.text((x, y), " - ", fg, font=font)
+
+	font_author = ImageFont.truetype(
+		str(font_path.absolute()), int(font_size - (0.3 * font_size))
+	)
+	author_width = draw.textsize(author, font_author)[0]
+	x = (width - author_width) // 2
+	draw.text((x, y + 20), author, fg, font=font_author, antialias=True)
+
+	_current_time = time.strftime("%H-%M-%S", time.localtime())
+	_current_date = date.today().strftime("%B-%d-%Y")
+	# Save The Image as a Png file
+	image.convert("RGB")
+	image.save(pp(GENERATED, f"{_current_date}_{_current_time}.png"))
+
+
+def generate_image(content: str, author: str, image: str = ""):
 	if handle_output_dir() is False:
 		logger.error("something went wrong while creating _GENERATED dir, exiting...")
 		exit(-1)
 
 	image_path: PosixPath = (
-		pick_random_image() if random is True else pick_first_image()
+		pick_random_image() if image == "" else pick_first_image(image)
 	)
 	font_path: PosixPath = get_font_file()
 
@@ -74,9 +165,9 @@ def generate_image(content: str, author: str, random: bool = True):
 	img = Convert(
 		quote=content,
 		author=author,
-		fg=(200, 200, 200),
+		fg=(255, 255, 255),
 		bg=bg,
-		font_size=32,
+		font_size=20,
 		font_type=str(font_path.absolute()),
 		width=width,
 		height=height,
--- automation/hadith_post_fajr/hadith_post_fajr.py
+++ automation/hadith_post_fajr/hadith_post_fajr.py
@@ -9,11 +9,13 @@ import requests
 from urllib.parse import urlencode, urljoin
 import json
 import os
+from pathlib import Path as pp
 from datetime import datetime
 from datetime import timedelta
 
-from create_image import generate_image
+from create_image import custom_image_generator
 from log_handling.log_handling import logger
+from utils.paths import RESOURCES
 
 BASE_URL = "https://api.sunnah.com/v1/"
 
@@ -37,12 +39,20 @@ VIRTUES = "virtues"
 
 CURRENT_COLLECTION_UNDER_USE = BUKHARI
 
+
 DAY_IT_ALL_STARTED = datetime(2024, 4, 7).date()  # Monday, 7th of Apr, 2024
 TODAY = datetime.now().date()  # current day
 SUNNAH_KEY = os.environ.get("SUNNAH_KEY", "")
 if SUNNAH_KEY == "":
 	logger.error("key SUNNAH_KEY not set in env, exiting...")
 	exit(-1)
+
+MAX_WORDS_IN_HADITH = 400
+GOOD_NUMBER_OF_WORDS_IN_ONE_IMAGE = 150
+IMAGE_SET_DIR = "hadith_backgroud_image_set"
+IMAGE_SET_PATH = pp(RESOURCES, IMAGE_SET_DIR)
+BIGGEST_IMAGE_FILE = pp(IMAGE_SET_PATH, "pinterest_806425877043089332.png")
+MAX_NUMBER_OF_PAGES = 4
 
 
 def get_collections(collection_name: str) -> dict:
@@ -84,6 +94,21 @@ def get_hadith(collection_data: dict, hadith_number: int) -> dict:
 	return data
 
 
+def get_random_hadith():
+	random_hadith_endpoint = "hadiths/random"
+	headers = {"Accept": "application/json", "X-API-Key": os.environ["SUNNAH_KEY"]}
+	url = form_url(endpoint=random_hadith_endpoint, params={})
+	response = requests.get(url, headers=headers)
+
+	if response.status_code < 200 and response.status_code >= 300:
+		logger.error(
+			f"something went wrong in {get_collections.__name__}'s GET request, exiting..."
+		)
+		exit(-1)
+	data = json.loads(response.text)
+	return data
+
+
 def form_url(endpoint: str, params: dict):
 	non_null_params = {}
 	for key, value in params.items():
@@ -97,8 +122,24 @@ def form_url(endpoint: str, params: dict):
 	return url
 
 
-# def filter_hadith_text(raw_hadith: str):
-# 	return raw_hadith
+def extract_hadith_data(hadith_data: dict) -> str:
+	hadith_english: dict = hadith_data.get("hadith", "")[0]
+	hadith_arabic: dict = hadith_data.get("hadith", "")[1]  # noqa: F841
+
+	collection: str = hadith_data.get("collection")
+	book: str = hadith_data.get("bookNumber")
+	hadith_number: str = hadith_data.get("hadithNumber")
+	hadith_source = f"{collection.upper()} {book} : {hadith_number}"
+	raw_hadith = hadith_english.get("body")
+
+	replace = {"ﷺ": "PBUH"}
+	remove = ["<p>", "</p>", "<b>", "</b>", "<br>", "</br>", "<br/>"]
+	for key, value in replace.items():
+		raw_hadith = raw_hadith.replace(key, value)
+	for rem in remove:
+		raw_hadith = raw_hadith.replace(rem, "")
+
+	return {"hadith": raw_hadith, "hadith_source": hadith_source}
 
 
 def main():
@@ -107,12 +148,59 @@ def main():
 	hadith_data = get_hadith(
 		collection_data=collection_data, hadith_number=hadith_number
 	)
-	hadith_english = hadith_data.get("hadith", "")[0]
-	hadith_arabic = hadith_data.get("hadith", "")[1]  # noqa: F841
 
-	# hadith_ready = filter_hadith_text(hadith_english)
+	extracted_hadith_data = extract_hadith_data(hadith_data)
+	number_of_words_in_hadith = len(extracted_hadith_data.get("hadith").split())
+	if number_of_words_in_hadith > MAX_WORDS_IN_HADITH:
+		if number_of_words_in_hadith > 4 * MAX_WORDS_IN_HADITH:
+			hadith_data = get_random_hadith()
+			extracted_hadith_data = extract_hadith_data(hadith_data)
+		else:
+			words_in_each_page = number_of_words_in_hadith // MAX_NUMBER_OF_PAGES
+			word_list = extracted_hadith_data.get("hadith").split()
+			for i, index in enumerate(
+				range(
+					0,
+					number_of_words_in_hadith - words_in_each_page,
+					words_in_each_page,
+				)
+			):
+				if i == 3:
+					hadith_for_page = " ".join(word_list[index:])
+				else:
+					hadith_for_page = " ".join(
+						word_list[index : index + words_in_each_page]
+					)
+				custom_image_generator(
+					quote=hadith_for_page,
+					image_path=BIGGEST_IMAGE_FILE,
+					author=extracted_hadith_data.get("hadith_source"),
+					fg=(255, 255, 255),
+				)
+			return
 
-	generate_image(content=hadith_english.get("body"), author="Bukhari", random=True)
+	custom_image_generator(
+		quote=extracted_hadith_data.get("hadith"),
+		# image_path="/home/xd/Documents/python_codes/twitter_automation/resources/hadith_backgroud_image_set/pinterest_2040762320649080.jpg",
+		author=extracted_hadith_data.get("hadith_source"),
+		fg=(255, 255, 255),
+	)
+
+	# # Depricated
+	# generate_image(content=pre_processed_hadith,
+	# 							author=CURRENT_COLLECTION_UNDER_USE,
+	# 							image="/home/xd/Documents/python_codes/twitter_automation/resources/hadith_backgroud_image_set/pinterest_842876886503797396.jpg")
+
+	# # to test stability
+	# for i in range(1, 50):
+	# 	hadith_data = get_hadith(
+	# 	collection_data=collection_data, hadith_number=i
+	# 	)
+	# 	extracted_hadith_data = extract_hadith_data(hadith_data)
+	# 	custom_image_generator(quote=extracted_hadith_data.get("hadith"),
+	# 												image_path="/home/xd/Documents/python_codes/twitter_automation/resources/hadith_backgroud_image_set/pinterest_2040762320649080.jpg",
+	# 												author=extracted_hadith_data.get("hadith_source"),
+	# 												fg=(255,255,255))
 
 
 if __name__ == "__main__":
