2639d5afb39d9eaf53a8647b93fb8957bc5c47f5
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2024-04-10 13:19:14 +0530

    Added comments, some positional changes

--- automation/hadith_post_fajr/hadith_post_fajr.py
+++ automation/hadith_post_fajr/hadith_post_fajr.py
@@ -13,6 +13,7 @@ from pathlib import Path as pp
 from datetime import datetime
 from datetime import timedelta
 import nltk
+import random
 
 from create_image import custom_image_generator
 from log_handling.log_handling import logger
@@ -20,6 +21,7 @@ from utils.paths import RESOURCES
 
 BASE_URL = "https://api.sunnah.com/v1/"
 
+# Hadith constants
 BUKHARI = "bukhari"
 MUSLIM = "muslim"
 NASAI = "nasai"
@@ -37,10 +39,9 @@ ADAB = "adab"
 BULUGH = "bulugh"
 HISN = "hisn"
 VIRTUES = "virtues"
-
 CURRENT_COLLECTION_UNDER_USE = BUKHARI
 
-
+# Cron constants
 DAY_IT_ALL_STARTED = datetime(2024, 4, 7).date()  # Monday, 7th of Apr, 2024
 TODAY = datetime.now().date()  # current day
 SUNNAH_KEY = os.environ.get("SUNNAH_KEY", "")
@@ -48,14 +49,33 @@ if SUNNAH_KEY == "":
 	logger.error("key SUNNAH_KEY not set in env, exiting...")
 	exit(-1)
 
-MAX_WORDS_IN_HADITH = 250
+# Image generation constants
+MAX_WORDS_IN_HADITH = 400
 GOOD_NUMBER_OF_WORDS_IN_ONE_IMAGE = 150
 IMAGE_SET_DIR = "hadith_backgroud_image_set"
 IMAGE_SET_PATH = pp(RESOURCES, IMAGE_SET_DIR)
-BIGGEST_IMAGE_FILE = pp(IMAGE_SET_PATH, "pinterest_806425877043089332.png")
+
+
+def _get_largest_files(
+	directory, n=10
+):  # Get list of n largest files in IMAGE_SET_PATH
+	dir_path = pp(directory)
+	files = [
+		(file, file.stat().st_size) for file in dir_path.iterdir() if file.is_file()
+	]
+	sorted_files = sorted(files, key=lambda x: x[1], reverse=True)
+	largest_files = sorted_files[:n]
+	return largest_files
+
+
+NUMBER_OF_BIGGEST_IMAGE_FILES = 10
+BIGGEST_IMAGE_FILE = random.choice(
+	_get_largest_files(IMAGE_SET_PATH, NUMBER_OF_BIGGEST_IMAGE_FILES)
+)[0]
 MAX_NUMBER_OF_PAGES = 4
 
 
+# GET available collections
 def get_collections(collection_name: str) -> dict:
 	collections_endpoint = "collections"
 	collections_params = {"limit": 50, "page": 1}
@@ -63,6 +83,7 @@ def get_collections(collection_name: str) -> dict:
 	url = form_url(endpoint=collections_endpoint, params=collections_params)
 	response = requests.get(url, headers=headers)
 
+	# Simple response handler
 	if response.status_code < 200 and response.status_code >= 300:
 		logger.error(
 			f"something went wrong in {get_collections.__name__}'s GET request, exiting..."
@@ -75,10 +96,12 @@ def get_collections(collection_name: str) -> dict:
 			return collection
 
 
+# Calibrate the delta date difference
 def calibrate_time_difference() -> timedelta:
 	return TODAY - DAY_IT_ALL_STARTED
 
 
+# GET hadith from collection data and hadith number
 def get_hadith(collection_data: dict, hadith_number: int) -> dict:
 	collection_name = collection_data.get("name", CURRENT_COLLECTION_UNDER_USE)
 	hadith_endpoint = f"collections/{collection_name}/hadiths/{hadith_number}"
@@ -86,6 +109,7 @@ def get_hadith(collection_data: dict, hadith_number: int) -> dict:
 	url = form_url(endpoint=hadith_endpoint, params={})
 	response = requests.get(url, headers=headers)
 
+	# Simple response handler
 	if response.status_code < 200 and response.status_code >= 300:
 		logger.error(
 			f"something went wrong in {get_collections.__name__}'s GET request, exiting..."
@@ -95,12 +119,14 @@ def get_hadith(collection_data: dict, hadith_number: int) -> dict:
 	return data
 
 
+# GET random hadith if current hadith is too big to post
 def get_random_hadith():
 	random_hadith_endpoint = "hadiths/random"
 	headers = {"Accept": "application/json", "X-API-Key": os.environ["SUNNAH_KEY"]}
 	url = form_url(endpoint=random_hadith_endpoint, params={})
 	response = requests.get(url, headers=headers)
 
+	# Simple response handler
 	if response.status_code < 200 and response.status_code >= 300:
 		logger.error(
 			f"something went wrong in {get_collections.__name__}'s GET request, exiting..."
@@ -110,7 +136,8 @@ def get_random_hadith():
 	return data
 
 
-def form_url(endpoint: str, params: dict):
+# URL maker utility
+def form_url(endpoint: str, params: dict) -> str:
 	non_null_params = {}
 	for key, value in params.items():
 		if value is None or value == "":
@@ -123,16 +150,19 @@ def form_url(endpoint: str, params: dict):
 	return url
 
 
-def extract_hadith_data(hadith_data: dict) -> str:
+# Hadith data extractor utility
+def extract_hadith_data(hadith_data: dict) -> dict:
 	hadith_english: dict = hadith_data.get("hadith", "")[0]
 	hadith_arabic: dict = hadith_data.get("hadith", "")[1]  # noqa: F841
 
+	# Extracting data
 	collection: str = hadith_data.get("collection")
 	book: str = hadith_data.get("bookNumber")
 	hadith_number: str = hadith_data.get("hadithNumber")
 	hadith_source = f"{collection.upper()} {book} : {hadith_number}"
 	raw_hadith = hadith_english.get("body")
 
+	# Replacing non-renderable symbols, and removing junk chars
 	replace = {"ﷺ": "PBUH"}
 	remove = ["<p>", "</p>", "<b>", "</b>", "<br>", "</br>", "<br/>"]
 	for key, value in replace.items():
@@ -150,6 +180,7 @@ def extract_hadith_data(hadith_data: dict) -> str:
 	}
 
 
+# Utility splits the big Hadith into n equal paragraphs
 def split_into_paragraphs(text, n):
 	nltk.download("punkt")
 	# Tokenize the text into sentences
@@ -164,33 +195,45 @@ def split_into_paragraphs(text, n):
 		paragraph = " ".join(sentences[i : i + sentences_per_paragraph])
 		paragraphs.append(paragraph)
 
+	# After
+	if (
+		len(paragraphs[-1].split()) < 0.3 * len(paragraphs[-2].split())
+		and len(paragraphs) > 1
+	):
+		paragraphs[-2] += paragraphs[-1]
+		paragraphs.pop()
+
 	return paragraphs
 
 
+# Main
 def main():
+	# GET collections
 	collection_data = get_collections(CURRENT_COLLECTION_UNDER_USE)
+	# Calibrate the delta date difference
 	hadith_number = calibrate_time_difference().days
+	# GET hadith
 	hadith_data = get_hadith(
 		collection_data=collection_data, hadith_number=hadith_number
 	)
 
+	# Extract Hadith
 	extracted_hadith_data = extract_hadith_data(hadith_data)
 
+	# Logic to create images depending on the size of Hadith
 	number_of_words_in_hadith = len(extracted_hadith_data.get("narration").split())
-	if number_of_words_in_hadith > MAX_WORDS_IN_HADITH:
-		if number_of_words_in_hadith > MAX_NUMBER_OF_PAGES * MAX_WORDS_IN_HADITH:
+	if number_of_words_in_hadith > MAX_WORDS_IN_HADITH:  # If cant fit it one page
+		if (
+			number_of_words_in_hadith > MAX_NUMBER_OF_PAGES * MAX_WORDS_IN_HADITH
+		):  # If cant fit in 4 pages
 			hadith_data = get_random_hadith()
 			extracted_hadith_data = extract_hadith_data(hadith_data)
 		else:
 			narrator = extracted_hadith_data.get("narrator")
 			narration = extracted_hadith_data.get("narration")
 			number_of_pages = len(narration.split(" ")) // MAX_WORDS_IN_HADITH  # <= 4
+			# Splitting the Hadith into paragraphs
 			list_of_paragraphs = split_into_paragraphs(narration, number_of_pages)
-			if len(list_of_paragraphs[-1].split()) < 0.3 * len(
-				list_of_paragraphs[-2].split()
-			):
-				list_of_paragraphs[-2] += list_of_paragraphs[-1]
-				list_of_paragraphs.pop()
 			for i, paragraph in enumerate(list_of_paragraphs):
 				if i != 0:
 					narrator = ""
@@ -229,5 +272,6 @@ def main():
 	# 												fg=(255,255,255))
 
 
+# Entry point
 if __name__ == "__main__":
 	main()
